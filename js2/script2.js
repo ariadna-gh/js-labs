b7cc6dbce0105397de3328eeb02ca41a53d49125
ariadna-gh <ariadna.yakovlev@krfk.kai.edu.ua>
2025-05-05 10:58:55 +0300

    Add files via upload

---
+++ js2/script2.js
@@ -0,0 +1,21 @@
+var a = 3;
+let b = 'hello.';
+const c = 1;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let student = {
+   name: 'Ariadna',
+   age: 18,
+   status: true
+}
+
+student.university = "КАІ";
+console.log(student);
+
+b = 785479;
+console.log(typeof b);
+
+c = 24;
